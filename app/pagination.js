9cc0dd45b15d5c833f47a6e95092347831fecdba
Flavio Garcia <piraz@candango.org>
2021-12-23 19:23:31 -0500

    Removed canjs pagination implementation.
    Refs: #113

--- app/pagination.js
+++ app/pagination.js
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-import { FazNavbar, FazPagination, FazPaginationElement } from "../faz";
+import { FazNavbar, FazPaginationElement } from "../faz";
 
 import { MainNavbar } from "../app/main-navbar";
