7a28ffbb451ffce2cd17ed3d773d350f775a290d
Flavio Garcia <piraz@candango.org>
2023-06-24 22:41:34 -0400

    chore(types): add attributes types
    Refs: #110

---
+++ types/index.d.ts
@@ -0,0 +1,23 @@
+// See: https://cutt.ly/RwyeQ7ZT
+
+interface FazElementItem extends HTMLElement {
+    fazid: string;
+}
+
+interface FazElementItemAttributes extends HTMLAttributes<T> {
+    fazid?: string;
+}
+
+interface FazNavElement extends FazElementItem {
+}
+
+declare global {
+    declare module "solid-js" {
+        namespace JSX {
+            interface IntrinsicElements {
+                'faz-nav': FazElementItemAttributes<FazNavElement>;
+                'faz-nav-item': HTMLDivElement;
+            }
+        }
+    }
+}
