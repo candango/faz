6fb83d0addab9a77d8be3b921efdd046f7dfffa5
Flavio Garcia <piraz@candango.org>
2023-07-02 19:08:29 -0400

    build(deps): remove html-react-parser, prop-types, solid-bootstrap

--- nav/nav-item.js
+++ nav/nav-item.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018-2022 Flávio Gonçalves Garcia
+ * Copyright 2018-2023 Flavio Garcia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 import {FazReactItem} from "../item";
 import React from 'react'
+import FazNavElement from "./nav";
 
 
 export default class FazNavItemReact extends FazReactItem {
