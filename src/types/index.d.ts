338c654276915611cbb851097e5f705e689faf34
Flavio Garcia <piraz@candango.org>
2024-05-30 13:14:44 -0400

    build: add showcase back to the project
    Also providing a house cleaning to the project, and get karma out
    of the project.
    
    Fix: #122
    Refs: #115

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,31 @@
+/**
+ * Copyright 2018-2024 Flavio Garcia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 // See: https://cutt.ly/RwyeQ7ZT
 
-export { randomId } from "../id";
-export { FazFormElement } from "../form";
-export { FazElementItem } from "../item";
-export { toBoolean } from "../values";
+export { randomId } from "../id"
+export { FazFormElement } from "../form"
+export { FazElementItem } from "../item"
+export { toBoolean } from "../values"
 
 interface FazElementItem extends HTMLElement {
-    fazid: string;
+    fazid: string
 }
 
 interface FazElementItemAttributes extends HTMLAttributes<T> {
-    fazid?: string;
+    fazid?: string
 }
 
 interface FazNavElement extends FazElementItem {
@@ -20,8 +35,8 @@ declare global {
     declare module "solid-js" {
         namespace JSX {
             interface IntrinsicElements {
-                'faz-nav': FazElementItemAttributes<FazNavElement>;
-                'faz-nav-item': HTMLDivElement;
+                'faz-nav': FazElementItemAttributes<FazNavElement>
+                'faz-nav-item': HTMLDivElement
             }
         }
     }
--- watch.mjs
+++ watch.mjs
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018-2023 Flavio Garcia
+ * Copyright 2018-2024 Flavio Garcia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ import * as esbuild from "esbuild";
 import { assets } from "./assets.mjs";
 import { entryPoints } from "./entryPoints.mjs";
 import { copy } from "esbuild-plugin-copy";
-import { solidPlugin } from "esbuild-plugin-solid";
 
 let ctx = await esbuild.context({
     entryPoints: entryPoints,
@@ -28,13 +27,24 @@ let ctx = await esbuild.context({
     treeShaking: true,
     sourcemap: true,
     logLevel: "info",
-    outdir: "dist",
+    outdir: "showcase/dist",
     legalComments: "none",
-    allowOverwrite: true,
+    allowOverwrite: false,
     plugins:[
-        copy(assets),
-        solidPlugin()
+        copy(assets)
     ]
 });
 
 await ctx.watch();
+
+await ctx.serve({
+    port: 8080,
+    servedir: "showcase",
+    onRequest: (args) => {
+        let logMessage = "";
+        for (let key in args) {
+            logMessage += key + ": " + args[key] + " ";
+        }
+        console.log(logMessage);
+    }
+})
