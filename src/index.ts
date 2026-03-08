ed7fec05ffb7e1ca2ed8691f7c46819a374a96ba
Flavio Garcia <piraz@candango.org>
2026-03-07 23:26:05 -0500

    feat(core): expose reactivity engine and implement subpath exports
    - Added 'bindReactive' and 'FazPaginator' to main entry point (src/index.ts)
    - Implemented subpath exports in package.json for modern bundlers
    - Ensured type declarations are correctly generated and exposed
    - Resolves blocking issues for downstream libraries (e.g., faz-bootstrap)
    
    Fixes: #168

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { FazElement } from "./element";
 export type { FazElementAttributes } from "./element-attributes";
 export { FazFormElement } from "./form";
 export { randomId, toBoolean } from "./values";
+export { bindReactive } from "./reactivity";
+export { FazPaginator } from "./paginator";
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,6 @@ export { FazElement } from "./element";
 export type { FazElementAttributes } from "./element-attributes";
 export { FazFormElement } from "./form";
 export { randomId, toBoolean } from "./values";
+export { bindReactive } from "./reactivity";
+export { FazPaginator } from "./paginator";
 //# sourceMappingURL=index.d.ts.map
