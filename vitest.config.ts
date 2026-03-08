02e054d2d1186a8e6c7f0c5ba51131b97e065fc1
Flavio Garcia <piraz@candango.org>
2026-03-07 21:45:45 -0500

    build(fix): lock css-color to resolve ESM/CJS interop errors
    - Added resolution for @asamuzakjp/css-color@4.1.1
    - Reverted experimental Vitest config changes
    - Fixes persistent ERR_REQUIRE_ESM in Node 21/22 environments
    
    Refs: #167

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,23 +5,5 @@ export default defineConfig({
     plugins: [solid()],
     resolve: {
         conditions: ["development", "browser"],
-    },
-    ssr: {
-        noExternal: [
-            "@csstools/css-calc",
-            "@asamuzakjp/css-color"
-        ]
-    },
-    test: {
-        environment: "jsdom",
-        pool: "threads",
-        server: {
-            deps: {
-                inline: [
-                    "@csstools/css-calc",
-                    "@asamuzakjp/css-color"
-                ]
-            }
-        }
     }
 })
